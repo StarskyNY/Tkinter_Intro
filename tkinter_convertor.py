b97d02d722c4fe2e7a0397786b8284273eb3896a
StarskyNY <star.eu88@gmail.com>
2016-11-16 11:07:53 -0500

    Update tkinter_convertor.py

--- tkinter_convertor.py
+++ tkinter_convertor.py
@@ -12,14 +12,10 @@ def converter():
 	t2.insert(END,grams)
 	t3.insert(END,ounces)
 
-#b1 = Button(window,text="Execute",command=km_to_mi)
-
 Label(window, text="kg").grid(row=0,column=1)
 Label(window, text="grams").grid(row=0,column=2)
 Label(window, text="pounds").grid(row=0,column=3)
 Label(window, text="ounces").grid(row=0,column=4)
-#Label(window, text="pounds").grid(column=1)
-#Label(window, text="ounces").grid(column=1)
 
 b1= Button(window,text="Convert",command=converter)
 b1.grid(row=1,column=0)
@@ -38,11 +34,5 @@ e1_input=StringVar()
 e1 = Entry(window,textvariable=e1_input)
 e1.grid(row=1,column=1)
 
-#t2=Text(window,height=1,width=20)
-#t2.grid(row=0,column=3)
-
-
 
 window.mainloop()
-
-#print help(t1.insert)
